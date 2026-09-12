f2520ce97e6bc6518ff5b5b52fc2640fb5c89a9a
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-12 15:19:39 -0400

    Create sceneChanger.py
    This will make it the scenes change when their index is called.

---
+++ sceneChanger.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import (QStackedWidget)
+
+class SceneChanger():
+  def __init__(self):
+    super().__init__(self)
+    self.stack = QStackedWidget()
+  def changeScene(self, scene):
+    self.stack.currentIndex(scene)
+    
