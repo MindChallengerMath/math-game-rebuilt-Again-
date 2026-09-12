d2f9f2b574fbccc26168bdda1dfa9dcd475d3ab3
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-12 18:09:50 -0400

    Creating mainMenu.py

--- scenes/mainMenu.py
+++ scenes/mainMenu.py
@@ -1,1 +1,19 @@
-from Pyside6.QtWidgets
+from Pyside6.QtWidgets import ( QWidget,
+                                QLabel,
+                                QPushButton,
+                                QVBoxLayout,
+                                QHBoxLayout)
+
+class MainMenu(QWidget):
+    def __init__(self):
+        super().__init__(self)
+        self.title = QLabel("Mental Math Master")
+        self.startButton = QPushButton("Start")
+        self.exitButton = QPushButton("Exit")
+        self.initUI()
+    def initUI(self):
+        self.vbox = QVBoxLayout()
+        self.hbox = QHBoxLayout()
+        self.vbox.addWidget(self.title)
+        self.vbox.addWidget(self.startButton)
+        self.vbox.addWidget(self.exitButton)
