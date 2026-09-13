2ce78742504c46ab71fd25483638aa6e8811a932
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-13 12:58:37 -0400

    Update main.py
    Made it so that widgets can now be put into the index

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from sceneChanger import SceneChanger
 from gameSetup.operations import Operations
 from scenes.mainMenu import MainMenu
+from PySide6 import (QApplication)
 import sys
 
 if __name__ == "__main__":
+  app = QApplication
   sceneChanger = SceneChanger()
   operations = Operations()
   mainMenu = MainMenu()
-  sceneChanger.stack.addWidget(mainMenu)
+  sceneChanger.stack.insertWidget(mainMenu)
+  sceneChanger.stack.show()
+  app.exec()
+  
   
