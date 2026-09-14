15959a9c3aa2d05e205834c01e0eacfd7e88cbcf
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-14 17:25:12 -0400

    Exit screen
    The exit will appear when the user clicks on the exit button, but the exit screen itself does not have functionality yet.

--- scenes/mainMenu.py
+++ scenes/mainMenu.py
@@ -7,16 +7,43 @@ from Pyside6.QtWidgets import ( QWidget,
 class MainMenu(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = QLabel("Mental Math Master")
+        self.title = "Mental Math Master"
+        self.label = QLabel(self.title)
+      
         self.startButton = QPushButton("Start")
+      
         self.exitButton = QPushButton("Exit")
+        self.exitButton.clicked.connect(self.exit)
+      
+        self.yesButton = QPushButton("Yes")
+      
+        self.noButton = QPushButton("No")
+      
         self.initUI()
     def initUI(self):
         self.vbox = QVBoxLayout()
         self.hbox = QHBoxLayout()
+      
         self.setLayout(self.vbox)
         self.setStyleSheet("""
         """)
+      
         self.vbox.addWidget(self.title)
         self.vbox.addWidget(self.startButton)
         self.vbox.addWidget(self.exitButton)
+
+        self.vbox.addLayout(self.hbox)
+        
+        self.hbox.addWidget(self.yesButton)
+        self.yesButton.hide()
+      
+        self.hbox.addWidget(self.noButton)
+        self.noButton.hide()
+    def exit(self):
+        self.label.setText("Do you want to exit?")
+        self.startButton.hide()
+        self.exitButton.hide()
+        self.yesButton.show()
+        self.noButton.show()
+    def no(self):
+        pass
