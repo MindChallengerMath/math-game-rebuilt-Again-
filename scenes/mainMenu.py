993b92eeab13bc559b0d0baf954e442b314425b0
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-15 21:20:41 -0400

    Finished mainMenu functionality
    The style will be started after all functionality is complete.

--- scenes/mainMenu.py
+++ scenes/mainMenu.py
@@ -1,23 +1,30 @@
-from Pyside6.QtWidgets import ( QWidget,
+from PySide6.QtWidgets import ( QWidget,
                                 QLabel,
                                 QPushButton,
                                 QVBoxLayout,
                                 QHBoxLayout)
-
+from PySide6.QtCore import (Qt)
+import sys
 class MainMenu(QWidget):
-    def __init__(self):
+    def __init__(self, sceneChanger):
+        #Super makes the MainMenu class inherit the function and variables
+        #from QWidgets
         super().__init__()
+        self.sceneChanger = sceneChanger
         self.title = "Mental Math Master"
         self.label = QLabel(self.title)
       
         self.startButton = QPushButton("Start")
+        self.startButton.clicked.connect(self.start)
       
         self.exitButton = QPushButton("Exit")
         self.exitButton.clicked.connect(self.exit)
       
         self.yesButton = QPushButton("Yes")
+        self.yesButton.clicked.connect(self.yes)
       
         self.noButton = QPushButton("No")
+        self.noButton.clicked.connect(self.no)
       
         self.initUI()
     def initUI(self):
@@ -28,7 +35,7 @@ class MainMenu(QWidget):
         self.setStyleSheet("""
         """)
       
-        self.vbox.addWidget(self.title)
+        self.vbox.addWidget(self.label)
         self.vbox.addWidget(self.startButton)
         self.vbox.addWidget(self.exitButton)
 
@@ -39,11 +46,19 @@ class MainMenu(QWidget):
       
         self.hbox.addWidget(self.noButton)
         self.noButton.hide()
+    def start(self):
+        self.sceneChanger.changeScene(1)
     def exit(self):
         self.label.setText("Do you want to exit?")
         self.startButton.hide()
         self.exitButton.hide()
         self.yesButton.show()
         self.noButton.show()
+    def yes(self):
+        sys.exit()
     def no(self):
-        pass
+        self.label.setText(self.title)
+        self.yesButton.hide()
+        self.noButton.hide()
+        self.startButton.show()
+        self.exitButton.show()
