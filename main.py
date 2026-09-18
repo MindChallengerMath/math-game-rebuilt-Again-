68d5948002b98d21c83d019db4941e8c125c49d3
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-17 21:12:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sceneChanger import SceneChanger
 from gameSetup.operations import Operations
 from scenes.mainMenu import MainMenu
 from scenes.difficultyMenu import DifficultyMenu
+from scenes.game import Game
 from PySide6 import (QApplication)
 import sys
 
@@ -11,8 +12,11 @@ if __name__ == "__main__":
   operations = Operations()
   mainMenu = MainMenu(sceneChanger)
   difficultyMenu = DifficultyMenu(sceneChanger)
+  game = Game(operations)
+  #This puts the widgets into the index
   sceneChanger.stack.insertWidget(0, mainMenu)
   sceneChanger.stack.insertWidget(1, difficultyMenu)
+  sceneChanger.stack.insertWidget(2, game)
   sceneChanger.stack.show()
   app.exec()
   
