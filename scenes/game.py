b52926e591d157f5d09044f94bf5cdea709eacd3
MindChallengerMath <mindchallengermath@gmail.com>
2026-09-17 21:18:49 -0400

    Update game.py

--- scenes/game.py
+++ scenes/game.py
@@ -1,1 +1,20 @@
-from PySide6.QtWidget import (QWidget)
+from PySide6.QtWidget import (QWidget,
+                              QLabel,
+                              QLineEdit,
+                              QPushButton,
+                              QVBoxLayout,
+                              QHBoxLayout)
+from PySide6.QtCore import Qt
+
+class Game(QWidget):
+    def __init__(self, operations):
+        self.operations = operations
+        self.score = 0
+        self.answer = 0
+        self.sign = ("+", "-", "*", "/")
+        self.label = QLabel("Equation")
+        self.userInput = QLineEdit()
+        self.button = QPushButton("Enter")
+        self.yesButton = QPushButton("Yes")
+        self.noButton = QPushButton("No")
+        
